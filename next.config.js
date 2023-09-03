347df3313e6c2490d70da419563dcc96b17b3bed
mahfuzurrahman01 <mahfuzurrahman01010101@gmail.com>
2023-09-03 16:08:06 +0600

    ready to deploy in cpanal

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['source.unsplash.com','www.smbtex.com','images.unsplash.com','img.freepik.com','media.istockphoto.com','trumpcrownhk.com']
-      },
+  images: {
+    domains: ['source.unsplash.com','www.smbtex.com','images.unsplash.com','img.freepik.com','media.istockphoto.com','trumpcrownhk.com'],
+    unoptimized:true,
+  },
+  output: 'export',
 }
 
 module.exports = nextConfig
